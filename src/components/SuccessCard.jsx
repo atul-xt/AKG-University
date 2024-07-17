87e8b738b5cc715c12f1605de5d9f9e19771549a
Atul <atul.xtt@gmail.com>
2024-07-17 21:12:05 +0530

    updated

--- src/components/SuccessCard.jsx
+++ src/components/SuccessCard.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 function SuccessCard({ title, description, image }) {
-    return (
-        <div
-            className="relative h-[450px] w-full flex items-end overflow-hidden text-center text-white shadow-lg shadow-black/30 bg-contain bg-no-repeat bg-center transition-transform duration-700 ease-out hover:items-center hover:bg-opacity-80 focus-within:items-center focus-within:bg-opacity-80"
-            style={{ backgroundImage: `url(${image})` }}
-        >
-            <div className="relative h-72 flex flex-col items-center w-full p-4 transition-transform duration-700 ease-out transform translate-y-56 hover:translate-y-0 focus-within:translate-y-0 hover:h-full hover:justify-center backdrop-blur-md bg-black/30">
-                <h2 className="text-lg font-bold">{title}</h2>
-                <p className="font-serif text-lg italic mt-4">{description}</p>
-                <button className="mt-6 px-4 py-2 text-sm font-bold uppercase bg-black hover:bg-gray-900 focus:outline-none">
-                    Full Story
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className="relative h-[450px] w-full flex items-end overflow-hidden text-center text-white shadow-lg shadow-black/30 bg-contain bg-no-repeat bg-center transition-transform duration-700 ease-out hover:items-center hover:bg-opacity-80 focus-within:items-center focus-within:bg-opacity-80"
+      style={{ backgroundImage: `url(${image})` }}
+    >
+      <div className="relative h-72 flex flex-col items-center w-full p-4 transition-transform duration-700 ease-out transform translate-y-56 hover:translate-y-0 focus-within:translate-y-0 hover:h-full hover:justify-center backdrop-blur-md bg-black/30">
+        <h2 className="text-lg font-bold">{title}</h2>
+        <p className="font-serif text-lg italic mt-4">{description}</p>
+        <button className="mt-6 px-4 py-2 text-sm font-bold uppercase bg-black hover:bg-gray-900 focus:outline-none">
+          Full Story
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default SuccessCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import SuccessCard from "../components/SuccessCard";
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.css';
 
 function Home() {
   const successDetails = [
@@ -59,54 +57,17 @@ function Home() {
     },
   ];
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
   return (
     <div className="pt-12">
-      
-        <h1 className="font-BarlowCondensed text-center font-medium tracking-wide text-3xl">Success Stories</h1>
+      <h1 className="font-BarlowCondensed text-center font-medium tracking-wide text-3xl">
+        Success Stories
+      </h1>
       <main className="w-full h-full p-10">
-        {/* <Slider {...settings} className="cursor-grab"> */}
-          {successDetails.map((details, index) => (
-            <div key={index} className="p-6">
-              <SuccessCard {...details} />
-            </div>
-          ))}
-      
-        {/* </Slider> */}
+        {successDetails.map((details, index) => (
+          <div key={index} className="p-6">
+            <SuccessCard {...details} />
+          </div>
+        ))}
       </main>
     </div>
   );
